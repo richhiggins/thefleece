e8d285cc25a63ca05f7ed43b708250233a72db43
Rich Higgins <rich.higgins@valtech.com>
2024-01-08 15:58:26 +0000

    added static build config and stubbed generateStaticParams for blog pages

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,13 +1,17 @@
 type Params = {
   params: {
-    slug: string
-  }
-}
+    slug: string;
+  };
+};
 
 export async function generateMetadata({ params }: Params) {
-  return { title: `Post: ${params.slug}` }
+  return { title: `Post: ${params.slug}` };
+}
+
+export async function generateStaticParams() {
+  return [{ slug: "foo" }];
 }
 
 export default function Page({ params }: Params) {
-  return <h1>Slug: {params.slug}</h1>
+  return <h1>Slug: {params.slug}</h1>;
 }
